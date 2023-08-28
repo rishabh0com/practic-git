2b3b353e5931572ced0b5212c09d3e0f47126df5
Mihir Soni <ms6711534@gmail.com>
2023-08-28 22:24:01 +0530

    I have updated the codes

--- prime.js
+++ prime.js
@@ -2,31 +2,12 @@ function prime(x)
 {
     let count = 0;
 
-    for(let i =1; i <= x; i++)
-    {
-        if(x % i == 0)
-        {
-            count++;
-        }
-    }
-
-    if(count == 2)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    for(let i =1; i <= x; i++) if(x % i == 0) count++;
+    if(count == 2) return true;
+    else return false;
 
 }
 
 let a = 13;
-if(prime(a) == true)
-{
-    console.log(a,"is a prime number");
-}
-else
-{
-    console.log(a,"is not a prime number");
-}
+if(prime(a) == true) console.log(a,"is a prime number");
+else console.log(a,"is not a prime number");
