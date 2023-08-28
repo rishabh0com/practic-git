b05f26926dc9c53a8557d8ae9faca99b3c5f10fb
Rishabh <rishabhtripathi587@gmail.com>
2023-08-28 22:15:13 +0530

    i created two file

---
+++ prime.js
@@ -0,0 +1,32 @@
+function prime(x)
+{
+    let count = 0;
+
+    for(let i =1; i <= x; i++)
+    {
+        if(x % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if(count == 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+let a = 13;
+if(prime(a) == true)
+{
+    console.log(a,"is a prime number");
+}
+else
+{
+    console.log(a,"is not a prime number");
+}
